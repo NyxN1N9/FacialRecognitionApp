695fa943892360c3d2f0bc3cfdf3594044e60baf
Nyx Neal <chaos648@gmail.com>
2023-11-16 14:26:08 -0600

    no git repo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.js'
 import './index.css'
 import "tachyons"
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />);
