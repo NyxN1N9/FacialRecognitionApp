1323dedbf7ca7547c031d093c176a03aa167f700
Nyx <chaos648@gmail.com>
2023-11-26 15:57:11 -0600

    upd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import {createRoot} from 'react-dom/client'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.js'
 import './index.css'
-import "tachyons"
+import 'tachyons'
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
